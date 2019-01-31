5b991b826b47b766a24fbbd01316ced1052a0647
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-01-31 23:57:40 +0700

    add display mode

--- TTGO-AQI/TTGO-AQI.ino
+++ TTGO-AQI/TTGO-AQI.ino
@@ -12,76 +12,79 @@ const char *password = "V4bTaMaTo";
 //OLED_RST -- GPIO16
 SSD1306 display(0x3c, 4, 15);
 
+#define DISPLAY_DURATION 3000
+typedef void (*screen)(void);
+int screenCount = 0;
+int counter = 1;
+
+const char* status;
+int data_aqi;
+float data_city_geo0;
+float data_city_geo1;
+const char* data_city_name;
+
 uint32_t pevTime = 0;
 
-void getAQI(String web, int timeDelay)
+void mainDisplay() {
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(0, 0, "ESP32 AQI");
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(0, 24, "City = ");
+  display.drawString(64, 24, data_city_name);
+  display.drawString(0, 40, "AQI = ");
+  display.drawString(48, 40, String(data_aqi));
+}
+
+void getAQI(String web)
 {
-  uint32_t curTime = millis();
-  if (curTime - pevTime > (timeDelay * 1000))
-  {
-    pevTime = curTime;
-    if (WiFi.status() == WL_CONNECTED)
-    { //Check WiFi connection status
-      HTTPClient http; //Declare an object of class HTTPClient
-      //    http.begin("http://jsonplaceholder.typicode.com/users/1");  //Specify request destination
+  HTTPClient http; //Declare an object of class HTTPClient
+  //    http.begin("http://jsonplaceholder.typicode.com/users/1");  //Specify request destination
 
-      //   http://aqicn.org/city/thailand/chiangmai---cmis/
-      http.begin(web);
+  //   http://aqicn.org/city/thailand/chiangmai---cmis/
+  http.begin(web);
 
-      int httpCode = http.GET(); //Send the request
-      if (httpCode > 0)
-      { //Check the returning code
-        String payload = http.getString(); //Get the request response payload
-        Serial.println(payload);           //Print the response payload
+  int httpCode = http.GET(); //Send the request
+  if (httpCode > 0)
+  { //Check the returning code
+    String payload = http.getString(); //Get the request response payload
+    Serial.println(payload);           //Print the response payload
 
-        // Allocate JsonBuffer
-        // Use arduinojson.org/assistant to compute the capacity.
-        //        const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
-        const size_t capacity = JSON_ARRAY_SIZE(2) + JSON_ARRAY_SIZE(3) + 12 * JSON_OBJECT_SIZE(1) + 4 * JSON_OBJECT_SIZE(2) + 2 * JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(8) + JSON_OBJECT_SIZE(11);
-        DynamicJsonBuffer jsonBuffer(capacity);
-        //        StaticJsonBuffer<1000> jsonBuffer;
-        JsonObject &root = jsonBuffer.parseObject(payload);
-        if (!root.success())
-        {
-          Serial.println(F("Parsing failed!"));
-          return;
-        }
-        Serial.println(F("Parsing success!"));
-
-        const char* status = root["status"]; // "ok"
-        JsonObject& data = root["data"];
-        int data_aqi = data["aqi"]; // 152
-
-        JsonObject& data_city = data["city"];
-        float data_city_geo0 = data_city["geo"][0]; // 18.787747
-        float data_city_geo1 = data_city["geo"][1]; // 98.9931284
-        const char* data_city_name = data_city["name"]; // "Chiang Mai"
-
-        Serial.println("ESP32 AQI ");
-        Serial.print("status : ");
-        Serial.println(status);
-        Serial.print("name : ");
-        Serial.print(data_city_name);
-        Serial.print(" AQI : ");
-        Serial.println(data_aqi);
-        Serial.print("Location : ");
-        Serial.print(data_city_geo0);
-        Serial.print(",");
-        Serial.println(data_city_geo1);
-
-        display.clear();
-        display.setFont(ArialMT_Plain_24);
-        display.drawString(0, 0, "ESP32 AQI");
-        display.setFont(ArialMT_Plain_16);
-        display.drawString(0, 24, "status = ");
-        display.drawString(64, 24, root["status"].as<char *>());
-        display.drawString(0, 40, "AQI = ");
-        display.drawString(48, 40, root["data"]["aqi"].as<char *>());
-        display.display();
-      }
-      http.end(); //Close connection
+    // Allocate JsonBuffer
+    // Use arduinojson.org/assistant to compute the capacity.
+    //        const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
+    const size_t capacity = JSON_ARRAY_SIZE(2) + JSON_ARRAY_SIZE(3) + 12 * JSON_OBJECT_SIZE(1) + 4 * JSON_OBJECT_SIZE(2) + 2 * JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(8) + JSON_OBJECT_SIZE(11);
+    DynamicJsonBuffer jsonBuffer(capacity);
+    //        StaticJsonBuffer<1000> jsonBuffer;
+    JsonObject &root = jsonBuffer.parseObject(payload);
+    if (!root.success())
+    {
+      Serial.println(F("Parsing failed!"));
+      return;
     }
+    Serial.println(F("Parsing success!"));
+
+    status = root["status"]; // "ok"
+    JsonObject& data = root["data"];
+    int data_aqi = data["aqi"]; // 152
+
+    JsonObject& data_city = data["city"];
+    float data_city_geo0 = data_city["geo"][0]; // 18.787747
+    float data_city_geo1 = data_city["geo"][1]; // 98.9931284
+    const char* data_city_name = data_city["name"]; // "Chiang Mai"
+
+    Serial.println("ESP32 AQI ");
+    Serial.print("status : ");
+    Serial.println(status);
+    Serial.print("name : ");
+    Serial.print(data_city_name);
+    Serial.print(" AQI : ");
+    Serial.println(data_aqi);
+    Serial.print("Location : ");
+    Serial.print(data_city_geo0);
+    Serial.print(",");
+    Serial.println(data_city_geo1);
   }
+  http.end(); //Close connection
 }
 
 void setup()
@@ -111,7 +114,28 @@ void setup()
   Serial.println("WiFi Connected...");
 }
 
+screen showOLED[] = {mainDisplay, timeDisplay};
+int screenLength = (sizeof(showOLED) / sizeof(screen));
+long timeSinceLastModeSwitch = 0;
+
 void loop()
 {
-  getAQI("http://api.waqi.info/feed/chiangmai/?token=c215825b5de34b7c75cd570d1cf2c1e957acc963", 30);
+  getAQI("http://api.waqi.info/feed/chiangmai/?token=c215825b5de34b7c75cd570d1cf2c1e957acc963");
+  // clear the display
+  display.clear();
+  // draw the current demo method
+  showOLED[screenCount]();
+
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  display.drawString(10, 128, String(millis()));
+  // write the buffer to the display
+  display.display();
+
+  if (millis() - timeSinceLastModeSwitch > DEMO_DURATION) {
+    screenCount = (screenCount + 1)  % screenLength;
+    timeSinceLastModeSwitch = millis();
+  }
+  counter++;
+  delay(10);
+
 }
